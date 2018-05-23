1257e57f0c42f3ca5d5de6d6e3632124957370b1
Nicolas Perez <pereznicolas90@gmail.com>
2018-05-23 01:21:07 -0500

    dankusss

--- Projecto_2/src/Arbol/s.java
+++ Projecto_2/src/Arbol/s.java
@@ -21,7 +21,7 @@ import javax.swing.JTree;
 import javax.swing.JComboBox;
 
 public class s extends JFrame {
-	
+
 	public static String texto;
 	private JPanel contentPane;
 	public static int cont = 0;
@@ -31,9 +31,10 @@ public class s extends JFrame {
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+		DefaultMutableTreeNode hey;
 			public void run() {
 				try {
-					s frame = new s();
+					s frame = new s(hey);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -53,153 +54,104 @@ public class s extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblVisualizadorArbolFamiliar = new JLabel("Visualizador Arbol Familiar");
 		lblVisualizadorArbolFamiliar.setFont(new Font("Lucida Sans", Font.BOLD, 17));
 		lblVisualizadorArbolFamiliar.setBounds(12, 13, 270, 29);
 		contentPane.add(lblVisualizadorArbolFamiliar);
-		
+
 		JTree tree = new JTree(hey);
 		tree.setFont(new Font("Lucida Sans", Font.PLAIN, 13));
 		tree.setBounds(12, 55, 368, 428);
 		contentPane.add(tree);
-		
+
 		JButton btnAgregarFamiliar = new JButton("Agregar Hijo");
 		btnAgregarFamiliar.addActionListener(new ActionListener() {
 			public void  actionPerformed(ActionEvent arg0) {
-				JTextField field1 = new JTextField();
-				JTextField field2 = new JTextField();
-				JTextField field3 = new JTextField();
-				Object[] fields = {
-						"Nombre ", field1, 
-						"Edad ", field2,
-						"Fecha Nacimiento ", field3
-				};
-				JOptionPane.showConfirmDialog(null, fields, "hi", JOptionPane.OK_CANCEL_OPTION);
-				/*emp upale = new emp();
-				upale.setVisible(true);	
-				upale.setWorking(true);
-			    TreePath path = tree.getSelectionPath();
-			
-			    DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
-			    tree.setSelectionRow(2);
-			    DefaultMutableTreeNode node =  (DefaultMutableTreeNode) path.getLastPathComponent();
-			    DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(upale.getPersona());
-			    DefaultMutableTreeNode dank = new DefaultMutableTreeNode("daml");
-			    model.insertNodeInto(newNode, node, node.getChildCount());
-			    */
-				
-			 
-				
+				String nombre, fechanac;
+				int edad;
+				nombre = JOptionPane.showInputDialog("Inserte el nombre");
+				edad = Integer.parseInt(JOptionPane.showInputDialog("Inserte la edad"));
+				fechanac = JOptionPane.showInputDialog("Inserte la fecha de nacimiento");
+				TreePath path = tree.getSelectionPath();
+				DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
+				tree.setSelectionRow(2);
+				DefaultMutableTreeNode node =  (DefaultMutableTreeNode) path.getLastPathComponent();
+				Persona dank  = new Persona(nombre, edad, fechanac);
+				Nodo dankus = new NodoIndividual(dank);
+				DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(dankus);
+				DefaultMutableTreeNode dink = new DefaultMutableTreeNode("lmao");
+				model.insertNodeInto(newNode, node, node.getChildCount());
 			}
 		});
-		
-		btnAgregarFamiliar.setBounds(407, 51, 139, 25);
+
+		btnAgregarFamiliar.setBounds(407, 51, 149, 25);
 		contentPane.add(btnAgregarFamiliar);
-		
+
 		JButton btnCambiarCentro = new JButton("Eliminar Nodo");
 		btnCambiarCentro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-                DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
+				DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
 
-                TreePath[] paths = tree.getSelectionPaths();
-                for (TreePath path : paths) {
-                    DefaultMutableTreeNode node = (DefaultMutableTreeNode) path.getLastPathComponent();
-                    if (node.getParent() != null) {
-                        model.removeNodeFromParent(node);
-                    }
-                }
-            }
+				TreePath[] paths = tree.getSelectionPaths();
+				for (TreePath path : paths) {
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) path.getLastPathComponent();
+					if (node.getParent() != null) {
+						model.removeNodeFromParent(node);
+					}
+				}
+			}
 		});
-		btnCambiarCentro.setBounds(407, 89, 139, 25);
+
+		btnCambiarCentro.setBounds(407, 89, 149, 25);
 		contentPane.add(btnCambiarCentro);
-		
+
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(407, 297, 139, 22);
+		comboBox.setBounds(407, 297, 149, 22);
 		contentPane.add(comboBox);
-		
+
 		JLabel lblMostrarPorRelacion = new JLabel("Mostrar por relaci\u00F3n...");
 		lblMostrarPorRelacion.setBounds(407, 268, 129, 16);
 		contentPane.add(lblMostrarPorRelacion);
-		
+
 		JLabel lblOrganizarPor = new JLabel("Organizar por....");
-		lblOrganizarPor.setBounds(407, 332, 129, 16);
+		lblOrganizarPor.setBounds(407, 400, 129, 16);
 		contentPane.add(lblOrganizarPor);
-		
+
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(407, 361, 139, 22);
+		comboBox_1.setBounds(407, 429, 149, 22);
 		contentPane.add(comboBox_1);
-		
+
 		JButton btnAgregarConyugue = new JButton("Agregar Conyugue");
 		btnAgregarConyugue.addActionListener(new ActionListener() {
 			public void  actionPerformed(ActionEvent arg0) {
-				
 				emp upale = new emp();
 				upale.setVisible(true);	
 				upale.setWorking(true);
-			    TreePath path = tree.getSelectionPath();
-			
-			    DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
-			    tree.setSelectionRow(2);
-			    DefaultMutableTreeNode node =  (DefaultMutableTreeNode) path.getLastPathComponent();
-			    DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(upale.getPersona());
-			    DefaultMutableTreeNode dank = new DefaultMutableTreeNode("daml");
-			    model.insertNodeInto(newNode, node, node.getChildCount());
-				
-			 
+				TreePath path = tree.getSelectionPath();
+				DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
+				tree.setSelectionRow(2);
+				DefaultMutableTreeNode node =  (DefaultMutableTreeNode) path.getLastPathComponent();
+				DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(upale.getPersona());
+				DefaultMutableTreeNode dank = new DefaultMutableTreeNode("daml");
+				model.insertNodeInto(newNode, node, node.getChildCount());
 			}
 		});
-		btnAgregarConyugue.setBounds(407, 127, 139, 25);
+		btnAgregarConyugue.setBounds(407, 127, 149, 25);
 		contentPane.add(btnAgregarConyugue);
 		
-	
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBounds(407, 332, 97, 25);
+		contentPane.add(btnAceptar);
 		
-	}
-	
-	private void createNodes(Nodo top) {
-	    Nodo centro = null;
-	    Nodo nodito = null;
-	    
-	    centro = new Nodo("hola");
-	    top = centro;
-	    
-	    //original Tutorial
-	    nodito = new Nodo("hola");
-	   
-//	    centro.setHermanos(new Nodo("holi"));
-	    
-	    /*//Tutorial Continued
-	    book = new DefaultMutableTreeNode(new BookInfo
-	        ("The Java Tutorial Continued: The Rest of the JDK",
-	        "tutorialcont.html"));
-	    category.add(book);
-	    
-	    //Swing Tutorial
-	    book = new DefaultMutableTreeNode(new BookInfo
-	        ("The Swing Tutorial: A Guide to Constructing GUIs",
-	        "swingtutorial.html"));
-	    category.add(book);
+		JButton btnMostrarInformacion = new JButton("Mostrar Informacion");
+		btnMostrarInformacion.setBounds(407, 165, 149, 25);
+		contentPane.add(btnMostrarInformacion);
 
-	    //...add more books for programmers...
-
-	    category = new DefaultMutableTreeNode("Books for Java Implementers");
-	    top.add(category);
-
-	    //VM
-	    book = new DefaultMutableTreeNode(new BookInfo
-	        ("The Java Virtual Machine Specification",
-	         "vm.html"));
-	    category.add(book);
-
-	    //Language Spec
-	    book = new DefaultMutableTreeNode(new BookInfo
-	        ("The Java Language Specification",
-	         "jls.html"));
-	    category.add(book);
-	    */
 	}
 
-public s(){
+
+	/*public s(){
 	lock lock = new lock();
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setBounds(100, 100, 602, 543);
@@ -207,17 +159,17 @@ public s(){
 	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 	setContentPane(contentPane);
 	contentPane.setLayout(null);
-	
+
 	JLabel lblVisualizadorArbolFamiliar = new JLabel("Visualizador Arbol Familiar");
 	lblVisualizadorArbolFamiliar.setFont(new Font("Lucida Sans", Font.BOLD, 17));
 	lblVisualizadorArbolFamiliar.setBounds(12, 13, 270, 29);
 	contentPane.add(lblVisualizadorArbolFamiliar);
-	
+
 	JTree tree = new JTree();
 	tree.setFont(new Font("Lucida Sans", Font.PLAIN, 13));
 	tree.setBounds(12, 55, 368, 428);
 	contentPane.add(tree);
-	
+
 	JButton btnAgregarFamiliar = new JButton("Agregar Hijo");
 	btnAgregarFamiliar.addActionListener(new ActionListener() {
 		public void  actionPerformed(ActionEvent arg0) {
@@ -226,22 +178,22 @@ public s(){
 			upale.setVisible(true);	
 			upale.setWorking(true);
 		    TreePath path = tree.getSelectionPath();
-		
+
 		    DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
 		    tree.setSelectionRow(2);
 		    DefaultMutableTreeNode node =  (DefaultMutableTreeNode) path.getLastPathComponent();
 		    DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(upale.getPersona());
 		    DefaultMutableTreeNode dank = new DefaultMutableTreeNode("daml");
 		    model.insertNodeInto(newNode, node, node.getChildCount());
-			
-		 
-			
+
+
+
 		}
 	});
-	
+
 	btnAgregarFamiliar.setBounds(407, 51, 139, 25);
 	contentPane.add(btnAgregarFamiliar);
-	
+
 	JButton btnCambiarCentro = new JButton("Eliminar Nodo");
 	btnCambiarCentro.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
@@ -258,23 +210,23 @@ public s(){
 	});
 	btnCambiarCentro.setBounds(407, 89, 139, 25);
 	contentPane.add(btnCambiarCentro);
-	
+
 	JComboBox comboBox = new JComboBox();
 	comboBox.setBounds(407, 297, 139, 22);
 	contentPane.add(comboBox);
-	
+
 	JLabel lblMostrarPorRelacion = new JLabel("Mostrar por relaci\u00F3n...");
 	lblMostrarPorRelacion.setBounds(407, 268, 129, 16);
 	contentPane.add(lblMostrarPorRelacion);
-	
+
 	JLabel lblOrganizarPor = new JLabel("Organizar por....");
 	lblOrganizarPor.setBounds(407, 400, 129, 16);
 	contentPane.add(lblOrganizarPor);
-	
+
 	JComboBox comboBox_1 = new JComboBox();
 	comboBox_1.setBounds(407, 429, 139, 22);
 	contentPane.add(comboBox_1);
-	
+
 	JButton btnAgregarConyugue = new JButton("Agregar Conyugue");
 	btnAgregarConyugue.addActionListener(new ActionListener() {
 		public void  actionPerformed(ActionEvent arg0) {
@@ -283,42 +235,42 @@ public s(){
 			upale.setVisible(true);	
 			upale.setWorking(true);
 		    TreePath path = tree.getSelectionPath();
-		
+
 		    DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
 		    tree.setSelectionRow(2);
 		    DefaultMutableTreeNode node =  (DefaultMutableTreeNode) path.getLastPathComponent();
 		    DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(upale.getPersona());
 		    DefaultMutableTreeNode dank = new DefaultMutableTreeNode("daml");
 		    model.insertNodeInto(newNode, node, node.getChildCount());
-			
-		 
+
+
 		}
 	});
 	btnAgregarConyugue.setBounds(407, 127, 139, 25);
 	contentPane.add(btnAgregarConyugue);
-	
+
 	JButton btnAceptar = new JButton("Aceptar");
 	btnAceptar.setBounds(407, 332, 97, 25);
 	contentPane.add(btnAceptar);
-	
+
 	JButton btnNewButton = new JButton("Mostrar Informacion");
 	btnNewButton.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			
+
 		}
 	});
 	btnNewButton.setBounds(407, 165, 139, 25);
 	contentPane.add(btnNewButton);
-	
 
-	
+
+
 }
-
+	 */
 
 }
 
 class lock {
-	
+
 	private boolean dank;
 	public lock() {
 		this.dank = true;
